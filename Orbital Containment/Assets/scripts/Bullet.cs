bde9c83a01ded11651a8f9f669ec9b175f841957
Alex Bronner <acbronn@gmail.com>
2019-08-22 19:19:27 -0400

    29.0 gitignore

---
+++ Orbital Containment/Assets/scripts/Bullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float velocity = 15f;
+    public float lifeRadius = 4.45f;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Rigidbody2D>().velocity = this.transform.up * velocity * -1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Debug.Log(this.transform.position.magnitude);
+		if(this.transform.position.magnitude > lifeRadius)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
